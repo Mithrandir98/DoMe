81119f277729d5111b89739aaeb69e0fc6d58354
Ruthwik H Param <aspire1998@gmail.com>
2021-01-21 17:02:02 +0700

    Final commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 //const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -104,7 +105,7 @@ app.post("/delete", (req, res) => {
 });
 
 app.get("/:customListName", (req, res) => {
-    const customListName = req.params.customListName;
+    const customListName = _.capitalize(req.params.customListName);
 
     List.findOne({name: customListName}, function(err, foundList){
         if (!err) {
